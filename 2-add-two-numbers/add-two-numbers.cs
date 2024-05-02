d8d86bb67dcd99af46b6cd7120ac711c722f3853
Van Hieu <57221471+vanhieu-it@users.noreply.github.com>
2024-05-02 14:04:54 +0700

    Create add-two-numbers.cs

---
+++ 2-add-two-numbers/add-two-numbers.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        int carry = 0;
+        ListNode cur = dummy;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        return dummy.next;
+    }
+}
