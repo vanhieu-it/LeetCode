50d2ba1f1f65bd756c5986eea7e79218e48f47b5
Van Hieu <57221471+vanhieu-it@users.noreply.github.com>
2024-05-02 14:03:47 +0700

    Create minimum-length-of-string-after-deleting-similar-ends.cs

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MinimumLength(string s) {
+        int i = 0, j = s.Length - 1;
+        while (i < j && s[i] == s[j]) {
+            while (i + 1 < j && s[i] == s[i + 1]) {
+                ++i;
+            }
+            while (i < j - 1 && s[j] == s[j - 1]) {
+                --j;
+            }
+            ++i;
+            --j;
+        }
+        return Math.Max(0, j - i + 1); 
+    }
+}
