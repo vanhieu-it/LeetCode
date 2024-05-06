798fa36393eb6ed1af354779a396f724785fad20
Van Hieu <57221471+vanhieu-it@users.noreply.github.com>
2024-05-06 10:03:35 +0700

    Create diameter-of-binary-tree.cs

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    private int ans;
+
+    public int DiameterOfBinaryTree(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        ans = Math.Max(ans, left + right);
+        return 1 + Math.Max(left, right);
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
