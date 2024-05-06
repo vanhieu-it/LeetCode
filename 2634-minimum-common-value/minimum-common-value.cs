f2f49583449bc2865e8892e3fa5dbba43586214a
Van Hieu <57221471+vanhieu-it@users.noreply.github.com>
2024-05-06 09:06:34 +0700

    Create minimum-common-value.cs

---
+++ 2634-minimum-common-value/minimum-common-value.cs
@@ -0,0 +1,23 @@
+class Solution
+{
+    public int getCommon(int[] nums1, int[] nums2)
+    {
+        int m = nums1.Length, n = nums2.Length;
+        for (int i = 0, j = 0; i < m && j < n;)
+        {
+            if (nums1[i] == nums2[j])
+            {
+                return nums1[i];
+            }
+            else if (nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
