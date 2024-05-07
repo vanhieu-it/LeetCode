9c72da874f16c086d0fbed3b70ad13896bb5537c
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-07 09:48:49 +0700

    up contains-duplicate

---
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        Array.Sort(nums);
+        for (int i = 0; i < nums.Length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
